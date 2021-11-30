f1798d073f6f7d4323d649709ea74df6e595ee2c
aks151 <kumar.ayush151@gmail.com>
2021-11-30 19:55:40 +0530

    trying to fix port issue

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import knex from 'knex';
     client: 'pg',
     connection: {
       host : '127.0.0.1',
-      port : 5432,
+      port : process.env.PORT,
       user : 'postgres',
       password : 'ayush1811',
       database : 'smartbrain'
